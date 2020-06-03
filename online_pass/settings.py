ce186c262777e10c502eca987caf0a7ce275325b
Kedar <56258764+ked1108@users.noreply.github.com>
2020-06-03 11:50:19 +0530

    Updated settings.py

--- online_pass/settings.py
+++ online_pass/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fw23+z4p865v8+!f(5&$^n5pqza+o6kozl3517bazzimss+c%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djangopasswordmanager.pythonanywhere.com"]
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_URL = "/login"
@@ -125,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
